3235f1926f5a3aff38ae4f90a7bdfeaa92dacb04
Taweechok Plajai <t_plajai_back@hotmail.co.th>
2020-02-22 09:15:40 +0700

    Add EF to project and Uility for connect dababase.

---
+++ CalendarEvent/Models/EventSchedulerViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarEvent.Models
+{
+    public class EventSchedulerViewModel
+    {
+		private CalendarEvent.EF.EventScheduler _ef;
+
+		public CalendarEvent.EF.EventScheduler ef
+		{
+			get { return _ef; }
+			set { _ef = value; }
+		}
+
+		public DateTime Eventdate { get; set; }
+
+		private string _strEventdate;
+
+		public string StrEventdate
+		{
+			get { return _strEventdate; }
+			set { _strEventdate = value; }
+		}
+
+		private int _Eventid;
+
+		public int Eventid
+		{
+			get { return _Eventid; }
+			set { _Eventid = value; }
+		}
+
+		private string _Title;
+
+		public string Title
+		{
+			get { return _Title; }
+			set {
+				_Title = value;
+
+				if (string.IsNullOrWhiteSpace(_Title))
+				{
+					_Title = "(No title)";
+				}
+			
+			}
+		}
+
+		private string _Description;
+
+		public string Description
+		{
+			get { return _Description; }
+			set { _Description = value; }
+		}
+
+
+
+
+	}
+}
---
+++ CalendarEvent/Utinity/AppConfig.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalendarEvent.Utinity
+{
+    public class AppConfig
+    {
+        public static string GetDBConnection(string connectionDBName)
+        {
+            try
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true).Build();
+                var conn = configuration.GetSection("Data:" + connectionDBName + ":ConnectionString");
+                return conn.Value.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("ErrorConfiguration at " + connectionDBName + " | Detail" + ex);
+            }
+        }
+    }
+}
