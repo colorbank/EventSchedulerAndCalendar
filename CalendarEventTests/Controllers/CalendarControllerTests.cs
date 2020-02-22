b07e099f78ffa51df3207015f810b0ff3af170de
Taweechok Plajai <t_plajai_back@hotmail.co.th>
2020-02-22 07:15:27 +0700

    Add project files.

---
+++ CalendarEventTests/Controllers/CalendarControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalendarEvent.Controllers.Tests
+{
+    [TestClass()]
+    public class CalendarControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
